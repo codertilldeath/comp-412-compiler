0ba26d7c287b8a65a77cf80367605915bc610de9
Benson Chu <bensonchu457@gmail.com>
2018-09-29 17:33:43 -0500

    Naive register allocation

--- src/backend/allocator.lisp
+++ src/backend/allocator.lisp
@@ -1,11 +1,70 @@
 (defpackage :allocator
   (:use :cl :alexandria)
   (:import-from :renamer
-                :rename-registers)
+                :rename-registers
+                :*VR-name*)
+  (:import-from :global
+                :*max-live*
+                :*max-register*)
   (:export :allocate-registers))
 
 
 (in-package :allocator)
 
+(defparameter *VR-to-PR* nil)
+(defparameter *PR-to-VR* nil)
+(defparameter *regs* nil)
+
+(defun number-list (iter num)
+  (if (= iter num)
+      '()
+      (cons iter (number-list (1+ iter) num))))
+
+(defun associate (vr pr)
+  (setf (aref *VR-to-PR* vr) pr)
+  (setf (aref *PR-to-VR* pr) vr))
+
+(defun disassociate (vr pr)
+  (setf (aref *VR-to-PR* vr) -1)
+  (setf (aref *PR-to-VR* pr) -1))
+
+(defun get-pr (vr)
+  (aref *VR-to-PR* vr))
+
+(defun allocate-safe (register)
+  (let ((v (ir::virtual register)))
+    (unless (= -1 v)
+      ;; First time using or defining VR
+      (when (= -1 (aref *VR-to-PR* v))
+        (associate v (pop *regs*)))
+      ;; Set the register
+      (setf (ir::physical register) (get-pr v)))))
+
+(defun clear-last-use (register)
+  (when (and (not (= -1 (ir::virtual register)))
+             (= -1 (ir::next-use register)))
+    (disassociate (ir::virtual register) (ir::physical register))
+    (push (ir::physical register) *regs*)))
+
+(defun allocate-full (ir registers)
+  (setf *VR-to-PR* (make-array *VR-name* :element-type 'fixnum :initial-element -1)
+        *PR-to-VR* (make-array registers :element-type 'fixnum :initial-element -1)
+        *regs* (number-list 0 registers))
+  (loop for i = (ll::head ir) then (ll::next i)
+     while i
+     for data = (ll::data i)
+     do
+       (progn
+         (allocate-safe (ir::r1 data))
+         (allocate-safe (ir::r2 data))
+         (clear-last-use (ir::r2 data))
+         (clear-last-use (ir::r1 data))
+         (allocate-safe (ir::r3 data)))))
+
 (defun allocate-registers (ir registers)
-  ir)
+  (rename-registers ir)
+  (if (<= registers *max-live*)
+      (allocate-full ir registers)
+      (progn
+        (print "SOL, buddy")
+        ir)))
--- src/test.lisp
+++ src/test.lisp
@@ -1,4 +1,36 @@
 
-(let* ((ir (parser:parse-file "../../../../students/lab2/code_check_1/cc5.i"))
-       (result (renamer:rename-registers ir)))
-  (ir::output-ir ir #'ir::virtual))
+(progn
+  (global:compile-start)
+  (let* ((ir (parser:parse-file "../../../../students/lab2/code_check_1/cc5.i")))
+    (renamer:rename-registers ir)
+    (ir::output-ir ir #'ir::virtual)))
+
+
+(progn
+  (global:compile-start)
+  (let* ((ir (parser:parse-file "../test/example.i")))
+    (renamer:rename-registers ir)
+    (ir::output-ir ir #'ir::virtual)))
+
+
+(progn
+  (global:compile-start)
+  (let* ((ir (parser:parse-file "../test/example.i")))
+    (allocator:allocate-registers ir 4)
+    (ir::output-ir ir #'ir::physical)))
+
+global:*max-live*
+global:*max-register*
+
+
+(declaim (inline popper))
+(defun popper (x)
+  (pop x))
+
+(defun aoeu ()
+  (let ((stack '(1 2 3)))
+    (popper stack)
+    (print stack)))
+
+(aoeu)
+
