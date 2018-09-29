6646d73f688e1cbe9fce8a379a914db2d092a4c8
Benson Chu <bensonchu457@gmail.com>
2018-09-29 14:07:55 -0500

    Testing cleaned update

--- src/backend/renamer.lisp
+++ src/backend/renamer.lisp
@@ -9,6 +9,17 @@
 (defparameter *last-use* nil)
 (defparameter *VR-name* nil)
 
+
+(defun update-reg (register count)
+  (let ((s (ir::source register)))
+    (unless (= s -1)
+      (when (= (aref *SR-to-VR* s) -1)
+        (setf (aref *SR-to-VR* s) *VR-name*)
+        (incf *VR-name*))
+      (setf (ir::virtual register) (aref *SR-to-VR* s))
+      (setf (ir::next-use register) (aref *last-use* s))
+      (setf (aref *last-use* s) count))))
+
 (defun update (ir count)
   (let ((s (ir::source (ir::r3 ir))))
     (unless (= s -1)
@@ -22,23 +33,8 @@
       (kill ir))
     ))
 
-  (let ((s (ir::source (ir::r2 ir))))
-    (unless (= s -1)
-      (when (= (aref *SR-to-VR* s) -1)
-        (setf (aref *SR-to-VR* s) *VR-name*)
-        (incf *VR-name*))
-      (setf (ir::virtual (ir::r2 ir)) (aref *SR-to-VR* s))
-      (setf (ir::next-use (ir::r2 ir)) (aref *last-use* s))
-      (setf (aref *last-use* s) count)))
-  
-  (let ((s (ir::source (ir::r1 ir))))
-    (unless (= s -1)
-      (when (= (aref *SR-to-VR* s) -1)
-        (setf (aref *SR-to-VR* s) *VR-name*)
-        (incf *VR-name*))
-      (setf (ir::virtual (ir::r1 ir)) (aref *SR-to-VR* s))
-      (setf (ir::next-use (ir::r1 ir)) (aref *last-use* s))
-      (setf (aref *last-use* s) count))))
+  (update-reg (ir::r2 ir) count)
+  (update-reg (ir::r1 ir) count))
 
 (defun kill (ir)
   (let ((s (ir::source (ir::r3 ir))))
