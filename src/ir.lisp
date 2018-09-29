4dfcb92db2cd21e502295244d0626800549a4822
Benson Chu <bensonchu457@gmail.com>
2018-09-29 13:59:51 -0500

    Added register struct

--- src/ir.lisp
+++ src/ir.lisp
@@ -7,22 +7,19 @@
 
 (in-package :ir)
 
+(defstruct (Register :conc-name)
+  (source -1 :type fixnum)
+  (virtual -1 :type fixnum)
+  (physical -1 :type fixnum)
+  (next-use -1 :type fixnum))
+
 (defstruct (IR :conc-name)
   (opcode "" :type string)
   (category nil :type symbol)
   (constant -1 :type fixnum)
-  (r1-s -1 :type fixnum)
-  (r1-v -1 :type fixnum)
-  (r1-p -1 :type fixnum)
-  (r1-nu -1 :type fixnum)
-  (r2-s -1 :type fixnum)
-  (r2-v -1 :type fixnum)
-  (r2-p -1 :type fixnum)
-  (r2-nu -1 :type fixnum)
-  (r3-s -1 :type fixnum)
-  (r3-v -1 :type fixnum)
-  (r3-p -1 :type fixnum)
-  (r3-nu -1 :type fixnum))
+  (r1 (make-Register) :type Register)
+  (r2 (make-Register) :type Register)
+  (r3 (make-Register) :type Register))
 
 ;; :memop :loadi :arithop :output :nop
 
@@ -43,15 +40,15 @@
 
 (defun register->int (r)
   (declare ((vector character *) r))
-  (str->int (subseq r 1)))
+  (make-Register :source (str->int (subseq r 1))))
 
 (defun make-memop (i)
   (destructuring-bind ((_o . opcode) (_r1 . reg1) (_i . _into) (_r2 . reg2) . _rest) i
     (declare (ignore _o _r1 _i _r2 _into _rest))
     (make-IR :opcode opcode
              :category :memop
-             :r1-s (register->int reg1)
-             :r3-s (register->int reg2))))
+             :r1 (register->int reg1)
+             :r3 (register->int reg2))))
 
 (defun make-loadi (i)
   (destructuring-bind ((_o . opcode) (_r1 . constant) (_i . _into) (_r2 . reg2) . _rest) i
@@ -59,16 +56,16 @@
     (make-IR :opcode opcode
              :category :loadi
              :constant (str->int constant)
-             :r3-s (register->int reg2))))
+             :r3 (register->int reg2))))
 
 (defun make-arithop (i)
   (destructuring-bind ((_o . opcode) (_r1 . r1) (_c . _comma) (_r2 . r2) (_i . _into) (_r3 . reg3) . _rest) i
     (declare (ignore _o _r1 _i _r2 _r3 _c _into _comma _rest))
     (make-IR :opcode opcode
              :category :arithop
-             :r1-s (register->int r1)
-             :r2-s (register->int r2)
-             :r3-s (register->int reg3))))
+             :r1 (register->int r1)
+             :r2 (register->int r2)
+             :r3 (register->int reg3))))
 
 (defun make-output (i)
   (destructuring-bind ((_o . opcode) (_c . constant) . _rest) i
@@ -92,9 +89,9 @@
   (format t "Opcode: ~a, Constant: ~a, Source: ~a, Source-aux: ~a, Destination: ~a~%"
           (opcode i)
           (constant i)
-          (r1-s i)
-          (r2-s i)
-          (r3-s i)))
+          (r1 i)
+          (r2 i)
+          (r3 i)))
 
 (defun output-virtual (ir)
   (loop for node = (ll::head ir) then (ll::next node)
@@ -102,8 +99,8 @@
      for data = (ll::data node)
      do
        (case (category data)
-         (:memop (format t "~a r~a => r~a~%" (opcode data) (r1-v data) (r3-v data)))
-         (:loadi (format t "~a ~a => r~a~%" (opcode data) (constant data) (r3-v data)))
-         (:arithop (format t "~a r~a, r~a => r~a~%" (opcode data) (r1-v data) (r2-v data) (r3-v data)))
+         (:memop (format t "~a r~a => r~a~%" (opcode data) (virtual (r1 data)) (virtual (r3 data))))
+         (:loadi (format t "~a ~a => r~a~%" (opcode data) (constant data) (virtual (r3 data))))
+         (:arithop (format t "~a r~a, r~a => r~a~%" (opcode data) (virtual (r1 data)) (virtual (r2 data)) (virtual (r3 data))))
          (:output (format t "output ~a~%" (constant data)))
          (:nop (format t "nop~%")))))
--- src/test.lisp
+++ src/test.lisp
@@ -1,4 +1,4 @@
 
-(let* ((ir (parser:parse-file "../../../../../students/lab2/code_check_1/cc5.i"))
+(let* ((ir (parser:parse-file "../../../../students/lab2/code_check_1/cc5.i"))
        (result (renamer:rename-registers ir 1000)))
   (ir::output-virtual ir))
