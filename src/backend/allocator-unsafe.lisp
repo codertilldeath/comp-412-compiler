996bef82666f39e756435f9c3c2fb67e888a2d4c
Benson Chu <bensonchu457@gmail.com>
2018-10-07 09:56:34 -0500

    PR-to-VR is no longer an array of structs

--- src/backend/allocator-unsafe.lisp
+++ src/backend/allocator-unsafe.lisp
@@ -1,27 +1,29 @@
 (in-package :allocator)
 
 (defparameter *VR-spilled?* nil)
+(defparameter *PR-next-use* nil)
 (defparameter *remat?* nil)
 
 (defun associate-unsafe (register pr)
   (setf (aref *VR-to-PR* (ir::virtual register)) pr)
-  (setf (aref *PR-to-VR* pr) register))
+  (setf (aref *PR-to-VR* pr) (ir::virtual register)))
 
 (defun disassociate-unsafe (register pr)
-  (setf (aref *VR-to-PR* (ir::virtual register)) -1)
-  (setf (aref *PR-to-VR* pr) (ir::make-register)))
-
+  (setf (aref *VR-to-PR* register) -1)
+  (setf (aref *PR-to-VR* pr) -1))
 
 (defun generate-spill (register regs)
   (let ((ll (ll:make-LL)))
     (ll:insert-back ll (ir::make-IR :opcode "loadI"
                                  :category :loadI
-                                 :constant (+ 32764 (* (ir::virtual register) 4))
+                                 :constant (+ 32764 (* (aref *PR-to-VR* register) 4))
                                  :r3 (ir::make-Register
                                       :physical (1- regs))))
     (ll:insert-back ll (ir::make-IR :opcode "store"
                                  :category :memop
-                                 :r1 register
+                                 :r1 (ir::make-Register
+                                      :virtual (aref *PR-to-VR* register)
+                                      :physical register)
                                  :r3 (ir::make-Register
                                       :physical (1- regs))
                                  :store t))
@@ -44,42 +46,41 @@
                                       :physical dest)))
     ll))
 
-(defun safety-check (linum)
-  (loop for i from 0 to (1- (car (array-dimensions *VR-to-PR*)))
-     do
-       (if (and (not (= -1 (aref *VR-to-PR* i)))
-                (not (= (ir::virtual (aref *PR-to-VR* (aref *VR-to-PR* i)))
-                        i)))
-           (format t "We have a problem! Line number ~a! vr~a does not match pr~a!~%" linum i (ir::virtual (aref *PR-to-VR* (aref *VR-to-PR* i))))))
-  (loop for i from 0 to (1- (car (array-dimensions *PR-to-VR*)))
-     do
-       (if (and (not (= -1 (ir::virtual (aref *PR-to-VR* i))))
-                (not (= (aref *VR-to-PR* (ir::virtual (aref *PR-to-VR* i)))
-                        i)))
-           (format t "We have a problem! Line number ~a! pr~a does not match vr~a!~%" linum i (aref *VR-to-PR* (ir::virtual (aref *PR-to-VR* i)))))))
+;; (defun safety-check (linum)
+;;   (loop for i from 0 to (1- (car (array-dimensions *VR-to-PR*)))
+;;      do
+;;        (if (and (not (= -1 (aref *VR-to-PR* i)))
+;;                 (not (= (ir::virtual (aref *PR-to-VR* (aref *VR-to-PR* i)))
+;;                         i)))
+;;            (format t "We have a problem! Line number ~a! vr~a does not match pr~a!~%" linum i (ir::virtual (aref *PR-to-VR* (aref *VR-to-PR* i))))))
+;;   (loop for i from 0 to (1- (car (array-dimensions *PR-to-VR*)))
+;;      do
+;;        (if (and (not (= -1 (ir::virtual (aref *PR-to-VR* i))))
+;;                 (not (= (aref *VR-to-PR* (ir::virtual (aref *PR-to-VR* i)))
+;;                         i)))
+;;            (format t "We have a problem! Line number ~a! pr~a does not match vr~a!~%" linum i (aref *VR-to-PR* (ir::virtual (aref *PR-to-VR* i)))))))
 
 (defun choose-spill-register (dont-use)
-  (loop for i from 0 to (1- (car (array-dimensions *PR-to-VR*)))
-     for max = (if (zerop dont-use)
-                   (list 1 (aref *PR-to-VR* 1))
-                   (list 0 (aref *PR-to-VR* 0)))
-     then
-       (let ((new (aref *PR-to-VR* i)))
-         (if (and (> (ir::next-use new)
-                     (ir::next-use (cadr max)))
-                  (not (= dont-use i)))
-             (list i new)
-             max))
-     finally
-       (return (car max))))
+  (let (max-next-use max-register) 
+    (loop for i from 0 to (1- (car (array-dimensions *PR-to-VR*)))
+       do
+         (unless (= dont-use i)
+           (if (null max-next-use)
+               (progn
+                 (setf max-next-use (aref *PR-next-use* i))
+                 (setf max-register i))
+               (when (> (aref *PR-next-use* i)
+                        max-next-use)
+                   (setf max-next-use (aref *PR-next-use* i))
+                   (setf max-register i)))))
+    max-register))
 
 (defun get-register-or-spill (ll ir rcount dont-use)
   (if-let (reg (pop *register-stack*))
     reg
     (let* ((reg (choose-spill-register dont-use))
            (to-spill (aref *PR-to-VR* reg)))
-      (disassociate-unsafe to-spill reg)
-      (if (aref *remat?* (ir::physical to-spill))
+      (if (aref *remat?* to-spill)
           ;; Rematerializeable
           (progn
             ;; Remove the loadI instruction
@@ -87,7 +88,8 @@
           ;; Regular spill
           (progn
             (ll:insert-before ll ir (generate-spill to-spill rcount))
-            (setf (aref *VR-spilled?* (ir::virtual to-spill)) t)))
+            (setf (aref *VR-spilled?* to-spill) t)))
+      (disassociate-unsafe to-spill reg)
       reg)))
 
 (defun allocate-unsafe (ll ir register rcount dont-use)
@@ -106,11 +108,12 @@
       ;; Update physical-register in IR
       (setf (ir::physical register) (get-pr v))
       ;; Update next-use, because the "when" above prevents uses from updating
-      (setf (aref *PR-to-VR* (get-pr v)) register))))
+      (setf (aref *PR-next-use* (get-pr v)) (ir::next-use register)))))
 
 (defun allocate-spill (ir registers)
   (setf *VR-to-PR* (make-array *VR-name* :element-type 'fixnum :initial-element -1)
-        *PR-to-VR* (make-array registers :element-type 'ir::register :initial-element (ir::make-register))
+        *PR-to-VR* (make-array registers :element-type 'fixnum :initial-element -1)
+        *PR-next-use* (make-array registers :element-type 'fixnum :initial-element -1)
         *VR-spilled?* (make-array *VR-name* :element-type 'boolean :initial-element nil)
         *remat?* (make-array *VR-name* :element-type 'boolean :initial-element nil)
         *register-stack* (number-list 0 (1- registers)))
--- src/test.lisp
+++ src/test.lisp
@@ -36,7 +36,7 @@
 (progn
   (global:compile-start)
   (let* ((ir (parser:parse-file "../../test/lab2_report/report5.i")))
-    (allocator:allocate-registers ir 4)
+    (allocator:allocate-registers ir 8)
     (ir::output-ir-comments ir)))
 
 (require 'sb-sprof)
