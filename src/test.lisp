777e9bbd4a3281817770ea49d57085a01a2f2c6a
Benson Chu <bensonchu457@gmail.com>
2018-11-19 16:07:44 -0600

    Comment this line out, just to see what would happen

--- src/test.lisp
+++ src/test.lisp
@@ -2,7 +2,7 @@
 
 (progn
   (global:compile-start)
-  (let* ((fname "../../test/lab3_report/report20.i")
+  (let* ((fname "../../test/lab3_report/report07.i")
          (ir (parser:parse-file fname)))
     (renamer:rename-registers ir)
     ;;(ir::output-ir ir #'ir::virtual)
